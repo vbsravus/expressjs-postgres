47721c238d3e7c8528f14d9256afa0d32e422886
Raí Raul <87578739+MonoDryad@users.noreply.github.com>
2022-11-01 16:23:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./src/app')
 
-const PORT = process.env.PORT || 7108
+const PORT = process.env.PORT || 7409
 
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${7108}/api `)
